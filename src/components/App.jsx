712f6507ed7865305dc95d5e6c05743bc2fd59f6
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2024-07-10 17:31:36 +0530

    CreateArea component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import Header from "./Header";
-import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes.js"
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map(item =>{
-        return <Note key= {item.id} title={item.title} content={item.content} />
-      })}
-      
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
