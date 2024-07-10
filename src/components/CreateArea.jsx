df674eb9ed0fc65fc1bc1b29f9696178bd65e15e
Diya Panchal <diya.pce21@sot.pdpu.ac.in>
2024-07-10 22:51:30 +0530

    icons added

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,40 +1,57 @@
 import React, { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
 
-    const[note, setNote] = useState({title: "", content: ""});
+  const [isExpanded, setExpanded] = useState(false);
 
-    function handleChange(event){
-        const{name,value} = event.target;
-        setNote((prevNote)=>{
-            return{
-                ...prevNote, [name] : value
-            }
-        });
-     }
+  function handleChange(event) {
+    const { name, value } = event.target;
 
-    function submitNote(event){
-        props.onAdd(note);
-        event.preventDefault();
-    }
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
 
   return (
     <div>
-      <form>
+      <form className="create-note">
         <input
-          onChange={handleChange}
           name="title"
+          onChange={handleChange}
           value={note.title}
           placeholder="Title"
         />
         <textarea
-          onChange={handleChange}
           name="content"
+          onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
           rows="3"
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={true}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 function Note(props) {
 
@@ -6,9 +7,15 @@ function Note(props) {
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button onClick={() => {
-        {props.onDelete(props.id);}
-      }}>DELETE</button>
+      <button
+        onClick={() => {
+          {
+            props.onDelete(props.id);
+          }
+        }}
+      >
+        <DeleteIcon />
+      </button>
     </div>
   );
 }
